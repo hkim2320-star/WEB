2d5a22d3255db082f5cad0e274c330da26088a99
Hyeonwoo Kim <h.kim.2320@westcliff.com>
2026-01-28 21:52:38 -0800

    change  button color

--- week1_day2_homework_module1/JS/randomcodes.js
+++ week1_day2_homework_module1/JS/randomcodes.js
@@ -1,16 +1,29 @@
-function generateCode() {
-    var code = "";
+    var code = ' ';
+    var getCode = ' ';
+    var btnvalue;
     var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
-    for (i=1; i<=8; i++) {
-        var char = Math.random() * str.length;
+
+function generateCode() {
+    
+    var code = "";
+
+    for (var i=1; i<=8; i++) {
+        var char = Math.floor(Math.random() * str.length);
         code += str.charAt(char);
     }
-    return code;
+}
+generateCode();
+document.getElementById("codes").innerHTML = code;
+
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue;
+
+    if (btnvalue == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
-document.getElementById("codes").innerHTML = generateCode();
-
-function disableButton() {
-    document.getElementById("submit").disabled = true;
-}
-disableButton();
--- week1_day2_homework_module1/JS/site_scripts.js
+++ week1_day2_homework_module1/JS/site_scripts.js
@@ -14,3 +14,5 @@
                 }
 
                 document.getElementById("partners").innerHTML = photos.join(" ");
+
+                
