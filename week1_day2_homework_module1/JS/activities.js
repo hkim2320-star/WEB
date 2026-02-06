bc4eee6c1039314098b7a0087b91116aa61d3017
Hyeonwoo Kim <h.kim.2320@westcliff.com>
2026-02-05 21:20:53 -0800

    Homework done

--- week1_day2_homework_module1/JS/activities.js
+++ week1_day2_homework_module1/JS/activities.js
@@ -8,3 +8,34 @@ $(document).ready(function () {
         $(this).toggleClass("selected");
     })
 })
+
+$(document).ready(function () {
+
+  $("td").click(function () {
+    var content = $(this).text();
+    
+
+
+    if (content != "Not Available") {
+        $(this).toggleClass("tdhighlight");
+    }
+
+    var colIndex = $(this).index();
+    var siteName = $("table tr:first-child").children().eq(colIndex).text();
+
+    if ($(this).hasClass("tdhighlight")) {
+
+      $("#displaySelected").css("visibility", "visible");
+      $("#displaySelected").css("margin-top", "2em");
+      $("#result").append("<p>" + content + " at " + siteName + "</p>");
+
+    } else {
+      $("#result p:contains('" + content + " at " + siteName + "')").remove();
+
+      if ($("#result").has("p").length == false) {
+        $("#displaySelected").css("visibility", "hidden");
+        $("#displaySelected").css("margin-top", "0");
+      }
+      }
+  });
+});
